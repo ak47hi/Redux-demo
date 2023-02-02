0fcef66923a756a736bc33c5c96579a4e860318c
Akhilesh Thurumalla <akhilesh,thurumalla@gmail.com>
2023-02-02 16:43:20 -0600

    To order and restock cakes and icecreams using a single reducer

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const redux = require("redux");
 
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
-const RESTOCK_CAKE = "RESTOCKCAKE";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
 
 // Action creator that returns the an object with type property
 function orderCake() {
@@ -12,18 +15,32 @@ function orderCake() {
     quantity: 1,
   };
 }
+function orderIcecream() {
+  return {
+    type: ICECREAM_ORDERED,
+    quantity: 1,
+  };
+}
 
+// Action creator to restock cakes
 function reStockCake(quantity = 1) {
   return {
-    type: RESTOCK_CAKE,
+    type: CAKE_RESTOCKED,
+    quantity: quantity,
+  };
+}
+
+function restockIcecream(quantity = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
     quantity: quantity,
   };
 }
 
 // Initial state of the object
 const initialState = {
+  numOfIcecream: 10,
   numOfCakes: 10,
-  anotherProp: 0,
 };
 
 // The reducer function takes in an initial state as default param to the state and an action
@@ -34,10 +51,20 @@ const reducer = (state = initialState, action) => {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
-    case RESTOCK_CAKE:
+    case CAKE_RESTOCKED:
       return {
         ...state,
         numOfCakes: state.numOfCakes + action.quantity,
+      };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIcecream: state.numOfIcecream - 1,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIcecream: state.numOfIcecream + action.quantity,
       };
     default:
       return state;
@@ -55,12 +82,23 @@ const unsubscribe = store.subscribe(() =>
   console.log("update state", store.getState())
 );
 
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(reStockCake(2));
-store.dispatch(reStockCake(2));
+const actions = bindActionCreators(
+  { orderCake, reStockCake, orderIcecream, restockIcecream },
+  store.dispatch
+);
 
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(reStockCake(2));
+// store.dispatch(orderIcecream());
+// store.dispatch(restockIcecream(2));
+
+actions.orderCake();
+actions.reStockCake(1);
+actions.orderIcecream();
+actions.orderIcecream();
+actions.restockIcecream(1);
 // after we unsubscribe we can no longer dipatch an action creator as the listener is already unsubscribed
 unsubscribe();
