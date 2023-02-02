217c03f3c1f10c80cadb193094d7fd6f17e9b446
Akhilesh Thurumalla <akhilesh,thurumalla@gmail.com>
2023-02-02 14:35:24 -0600

    action, reducer and store

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const redux = require("redux");
+
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 // Action creator that returns the an object with type property
@@ -25,3 +29,17 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("update state", store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+unsubscribe();
